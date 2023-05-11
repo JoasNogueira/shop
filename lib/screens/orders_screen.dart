80f5780091f3536189a7c2c4875304fd36ad6b36
Joas Magalhaes <joas.nogueira@inema.ba.gov.br>
2023-05-11 16:39:59 -0300

    Estilizando tela de pedidos

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,15 +1,23 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/order.dart';
+import 'package:shop/models/order_list.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final orders = Provider.of<OrderList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (context, index) => OrderWidget(order: orders.items[index])
       ),
     );
   }
