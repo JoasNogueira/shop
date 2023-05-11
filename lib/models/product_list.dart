d59c70bea975c47c6898d927e48ae8ce339a2e7c
Joas Magalhaes <joas.nogueira@inema.ba.gov.br>
2023-05-10 21:55:53 -0300

    Função para Filtrar os favoritos, adicionado  componente carrinho de compras e item do carrinho

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -3,9 +3,26 @@ import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final List<Product> _items = dummyProducts;
+  List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if(_showFavoriteOnly){
+      return [..._items.where((e) => e.isFavorite).toList()];
+    }
+    
+    return [..._items];
+  }
+
+  void showFavoriteOnly(){
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll(){
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
